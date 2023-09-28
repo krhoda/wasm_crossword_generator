21c1662ce3e811c2adc859e2bc236d17cdacbaef
K <kelseydrhoda@gmail.com>
2023-09-28 15:57:40 -0600

    Started working on placement logic

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,8 +1,13 @@
 mod utils;
 
-use rand::{seq::SliceRandom, thread_rng};
+use rand::{
+    distributions::{Distribution, Standard},
+    seq::SliceRandom,
+    thread_rng, Rng,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str, to_string};
+use thiserror::Error;
 use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
@@ -13,11 +18,29 @@ pub struct Word {
     pub clue: Option<String>,
 }
 
-#[derive(Clone, Deserialize, Serialize, Tsify)]
+#[derive(Clone, Debug, Deserialize, Serialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
 pub enum Direction {
     Horizontal,
     Verticle,
+}
+
+impl Direction {
+    pub fn other(&self) -> Direction {
+        match self {
+            Direction::Horizontal => Direction::Verticle,
+            Direction::Verticle => Direction::Horizontal,
+        }
+    }
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0..=1) {
+            0 => Direction::Horizontal,
+            _ => Direction::Verticle,
+        }
+    }
 }
 
 #[derive(Clone, Deserialize, Serialize, Tsify)]
@@ -27,7 +50,7 @@ pub struct PlacedWord {
     pub word: Word,
 }
 
-#[derive(Clone, Deserialize, Serialize, Tsify)]
+#[derive(Copy, Clone, Deserialize, Serialize, Tsify)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
 pub struct Space {
     pub letter: Option<Option<char>>,
@@ -67,8 +90,68 @@ pub struct Crossword<const W: usize, const H: usize> {
     pub words: Vec<Word>,
 }
 
+#[derive(Error, Debug)]
+pub enum CrosswordError {
+    #[error("word doesn't fit")]
+    BadFit,
+    #[error("point out of bounds")]
+    PointOutOfBounds,
+    #[error("intersection point can only be empty for placement of first word")]
+    EmptyIntersection,
+}
+
 impl<const W: usize, const H: usize> Crossword<W, H> {
-    fn new() -> Crossword<W, H> {
+    pub fn new(words: Vec<Word>, max_words: usize) -> Crossword<W, H> {
+        let mut crossword = Crossword::<W, H>::new_empty();
+        let mut words = words;
+        words.shuffle(&mut thread_rng());
+
+        for word in words {
+            // If max words hit, break.
+            if crossword.words.len() >= max_words {
+                break;
+            }
+
+            if crossword.words.is_empty() {
+                let _ = crossword.place(word, 0, 0);
+                continue;
+            }
+
+            let count_at_current_word = crossword.words.len();
+            for letter in word.text.chars() {
+                // TODO: Find a cleaner way to break out?
+                if count_at_current_word != crossword.words.len() {
+                    break;
+                }
+
+                // Clone to avoid borrow issues.
+                let current_puzzle = crossword.puzzle.clone();
+
+                for (row_count, _) in current_puzzle.iter().enumerate() {
+                    if count_at_current_word != crossword.words.len() {
+                        break;
+                    }
+
+                    for (col_count, _) in current_puzzle[row_count].row.iter().enumerate() {
+                        if count_at_current_word != crossword.words.len() {
+                            break;
+                        }
+
+                        if let Some(Some(c)) = current_puzzle[row_count].row[col_count].letter {
+                            if c == letter {
+                                // TODO: VERIFY THIS ISN'T BACKWARDS!
+                                let _ = crossword.place(word.clone(), col_count, row_count);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        crossword
+    }
+
+    fn new_empty() -> Crossword<W, H> {
         let puzzle: [CrosswordRow<W>; H] = std::array::from_fn(|_| CrosswordRow::<W>::new());
         Crossword {
             puzzle,
@@ -76,72 +159,53 @@ impl<const W: usize, const H: usize> Crossword<W, H> {
         }
     }
 
-    fn place(&self, word: Word, x: usize, y: usize, direction: Direction) {
-        // TODO: Something!
-    }
-
-    fn can_place(&self, word: &Word, x: usize, y: usize) -> Option<Placement> {
-        // TODO: Something!
-        None
-    }
-}
-
-pub fn gen_crossword_first_draft<const W: usize, const H: usize>(
-    words: Vec<Word>,
-    max_words: usize,
-) -> Crossword<W, H> {
-    let puzzle = Crossword::<W, H>::new();
-
-    let mut words = words;
-    words.shuffle(&mut thread_rng());
-
-    let mut word = words.pop();
-    if word.is_none() {
-        return puzzle;
-    }
-
-    puzzle.place(word.unwrap(), 0, 0, Direction::Horizontal);
-
-    let mut word_count: usize = 1;
-    word = words.pop();
-
-    while word.is_some() && word_count < max_words {
-        let w = word.unwrap();
-        let count_at_current_word = word_count;
-        for letter in w.text.chars() {
-            // TODO: Find a cleaner way to break out?
-            if count_at_current_word != word_count {
-                break;
-            }
-            for (row_count, _) in puzzle.puzzle.iter().enumerate() {
-                if count_at_current_word != word_count {
-                    break;
-                }
-                for (col_count, _) in puzzle.puzzle[row_count].row.iter().enumerate() {
-                    if count_at_current_word != word_count {
-                        break;
-                    }
-                    if let Some(Some(c)) = puzzle.puzzle[row_count].row[col_count].letter {
-                        if c == letter {
-                            // TODO: VERIFY THIS ISN'T BACKWARDS!
-                            if let Some(placement) = puzzle.can_place(&w, col_count, row_count) {
-                                puzzle.place(
-                                    w.clone(),
-                                    placement.x,
-                                    placement.y,
-                                    placement.direction,
-                                );
-                                word_count += 1;
-                            }
-                        }
-                    }
+    fn is_empty(&self) -> bool {
+        for row in self.puzzle.iter() {
+            for space in row.row {
+                if let Some(Some(_)) = space.letter {
+                    return false;
                 }
             }
         }
-        word = words.pop();
+
+        true
     }
 
-    puzzle
+    // place takes a word and a possible intersection of the word.
+    // The first word is special cased.
+    fn place(&mut self, word: Word, x: usize, y: usize) -> Result<(), CrosswordError> {
+        if x > W || y > H {
+            return Err(CrosswordError::PointOutOfBounds);
+        }
+
+        let row = self.puzzle[y].row;
+        let intersection = row[x];
+
+        if self.is_empty() {
+            // TODO: Something!
+            // let mut direction: Direction = rand::random();
+            // if !self.can_place(word, x, y, direction) {
+            //     direction = direction.other();
+            //     if !self.can_place(word, x, y, direction) {
+            //         return Err(CrosswordError::BadFit);
+            //     }
+            // }
+
+            // let chars = word.text.chars();
+
+            return Ok(());
+        }
+
+        if let Some(Some(letter)) = intersection.letter {
+            Ok(())
+        } else {
+            Err(CrosswordError::EmptyIntersection)
+        }
+    }
+
+    fn can_place(&mut self, word: Word, x: usize, y: usize) -> bool {
+        false
+    }
 }
 
 // Below here is the demo code
